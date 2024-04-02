95b82aecd528ec4d8903174b06509389667c5d90
root <root@cv3682165.novalocal>
2024-04-02 18:26:35 +0300

    Изменения в файле page.tsx и yarn.lock

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,46 @@
-export default function Home(){
-    return(
-          <div className="container">
-              <div className="product_block">
-                  <div className="product_item">
-                       <div className="product_item_img">
-                       <img src="/p1.jpg" alt="" />
-                       </div>
-
-                       <div className="product_item_info">
-                         <div className="product_item_title">Ботинки</div>
-                         <div className="product_item_price">6 800 Р</div>
-                       </div>
-
-                       <div className="product_button">В корзину</div>
-                  </div>
-
-                  <div className="product_item">
-                       <div className="product_item_img">
-                       <img src="/p2.jpg" alt="" />
-                       </div>
-
-                       <div className="product_item_info">
-                         <div className="product_item_title">Куртка</div>
-                         <div className="product_item_price">12 200 Р</div>
-                       </div>
-
-                       <div className="product_button">В корзину</div>
-                  </div>
-
-                  <div className="product_item">
-                       <div className="product_item_img">
-                       <img src="/p3.jpg" alt="" />
-                       </div>
-
-                       <div className="product_item_info">
-                         <div className="product_item_title">Рюкзак</div>
-                         <div className="product_item_price">5 300 Р</div>
-                       </div>
-
-                       <div className="product_button">В корзину</div>
-                  </div>
-              </div>
+export default function Home() {
+  return (
+    <div className="container">
+      <div className="product_block">
+        <div className="product_item">
+          <div className="product_item_img">
+            <img src="/p1.jpg" alt="" />
           </div>
-    );
+
+          <div className="product_item_info">
+            <div className="product_item_title">Ботинки</div>
+            <div className="product_item_price">6 800 Р</div>
+          </div>
+
+          <div className="product_button">В корзину</div>
+        </div>
+
+        <div className="product_item">
+          <div className="product_item_img">
+            <img src="/p2.jpg" alt="" />
+          </div>
+
+          <div className="product_item_info">
+            <div className="product_item_title">Куртка</div>
+            <div className="product_item_price">12 200 Р</div>
+          </div>
+
+          <div className="product_button">В корзину</div>
+        </div>
+
+        <div className="product_item">
+          <div className="product_item_img">
+            <img src="/p3.jpg" alt="" />
+          </div>
+
+          <div className="product_item_info">
+            <div className="product_item_title">Рюкзак</div>
+            <div className="product_item_price">5 300 Р</div>
+          </div>
+
+          <div className="product_button">В корзину</div>
+        </div>
+      </div>
+    </div>
+  );
 }
